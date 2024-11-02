d79accbbc5f3e7bd7a5a6a9c6d460ece29edd54b
sohyun022 <sohyun22@ajou.ac.kr>
2024-11-03 04:05:56 +0900

    Feat: add global exception handler for error handling

---
+++ src/main/java/doit/week4/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package doit.week4.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        return "예상치 못한 오류가 발생했습니다.";
+    }
+}
